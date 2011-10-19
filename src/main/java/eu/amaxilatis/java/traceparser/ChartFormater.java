7d16c3df89c286abc2142d31309851be7e109998
Dimitrios <amaxilat@cti.gr>
2011-10-19 23:36:48 +0300

    add a tab for graphics customization

---
+++ src/main/java/eu/amaxilatis/java/traceparser/ChartFormater.java
@@ -0,0 +1,11 @@
+package eu.amaxilatis.java.traceparser;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: amaxilatis
+ * Date: 10/19/11
+ * Time: 5:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChartFormater {
+}
